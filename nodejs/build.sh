cbe6628a42f93415c39c495f790e63aec641070e
chuongnh140 <chuongnh140@gmail.com>
2021-01-09 10:11:07 +0700

    end test

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -6,8 +6,8 @@ _dockerimage=chuongnh140/hello-node
 _dockertag=latest
 _dockerlogin=chuongnh140
 _dockerpasswd=nhcnhc1020@@
-cd nodejs && docker build -t $_dockerimage:$1 .
-docker tag $_dockerimage:$1 $_dockerimage:$_dockertag
+cd nodejs && docker build -t $_dockerimage .
+docker tag $_dockerimage $_dockerimage:$_dockertag
 echo $_dockerpasswd | docker login --username $_dockerlogin --password-stdin
 docker push $_dockerimage:$_dockertag
 if [[ $? -eq 0 ]]; then
