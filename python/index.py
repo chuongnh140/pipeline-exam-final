56a8c5ffae31ec281ab60db593b6ddfd928cc269
chuongnh140 <chuongnh140@gmail.com>
2021-01-25 05:08:30 +0700

    OK

--- python/index.py
+++ python/index.py
@@ -9,6 +9,6 @@ HOST=os.getenv('HOST_NAME')
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World! Application version {0} - Hostname {1}".format(TAG,HOST)  
+    return "Hello World!!! Application version {0} - Hostname {1}".format(TAG,HOST)  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
