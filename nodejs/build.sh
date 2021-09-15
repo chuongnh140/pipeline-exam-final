3ee7c45f7a9531ddec6bf5aa491727febd204ec4
chuongnh140 <75369541+chuongnh140@users.noreply.github.com>
2021-09-15 15:51:35 +0700

    Update build.sh

--- nodejs/build.sh
+++ nodejs/build.sh
@@ -6,7 +6,7 @@ _tagnow=$1
 _dockerimage=chuongnh140/hello-node
 _dockertag=latest
 _dockerlogin=chuongnh140
-_dockerpasswd=nhcnhc1020@@
+_dockerpasswd=-----------
 cd nodejs && docker build -t $_dockerimage:$_tagnow .
 docker tag $_dockerimage:$_tagnow $_dockerimage:$_dockertag
 echo $_dockerpasswd | docker login --username $_dockerlogin --password-stdin
